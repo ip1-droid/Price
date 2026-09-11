5ef97b4f19f84c82d3c194793ed3a9b16b6bc09f
ip1-droid <mohamadreza.1234ip1@gmail.com>
2026-09-11 11:28:27 +0330

    Set BOT_TOKEN for Telegram API
    Updated BOT_TOKEN to a specific value.

--- Price.py
+++ Price.py
@@ -25,7 +25,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # ========== تنظیمات ==========
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"  # جایگزین کنید
+BOT_TOKEN = "8947567937:AAHvDXs3oTrEzF-4pTtKMnb4r0BDbsXLuTU"  # جایگزین کنید
 TELEGRAM_API_URL = f"https://api.telegram.org/bot{BOT_TOKEN}"
 UPDATE_OFFSET = 0
 
